fa980d90ab35503856c5eb23bd2e0970d0da206f
keysafehub <postmaster@keysafehub.eu>
2026-07-15 09:23:30 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 import { Header } from '@/components/header'
 import { Footer } from '@/components/footer'
+// Importa il componente Script per caricare il Pixel in modo ottimizzato
+import Script from 'next/script'
 
 const inter = Inter({ 
   subsets: ["latin"],
@@ -70,12 +72,44 @@ export default function RootLayout({
   return (
     <html lang="it" className="bg-background">
       <head>
+        {/* JSON-LD Schema originale */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        
+        {/* Tag Noscript di riserva per utenti senza Javascript */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2081996525687917&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
+        {/* Meta Pixel Code caricato in modo non bloccante dopo l'interattività della pagina */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2081996525687917');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
         <Header />
         <main className="min-h-screen">
           {children}
